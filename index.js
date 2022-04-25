c472eed2360fbb573fb433f40dcf7a00477202a8
Liam <backgroundpositioncenter@gmail.com>
2022-04-25 19:11:04 +0300

    Сделал первичную верстку - HTML/CSS

---
+++ index.js
@@ -0,0 +1,7 @@
+const dom = {
+  testName: document.getElementById("test-name"),
+  questionNumber: document.getElementById("question-number"),
+  question: document.getElementById("question"),
+  answers: document.getElementById("answers"),
+  btn: document.getElementById("btn"),
+};
