4eae7d88ed20a6b91aff52a046a72885dbc611a2
Liam <backgroundpositioncenter@gmail.com>
2022-04-27 16:22:54 +0300

    Добавил функции проверки результатов тестирования и завершения теста

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const dom = {
   testName: document.getElementById("test-name"),
   questionNumber: document.getElementById("question-number"),
+  questionBlock: document.getElementById("question-block"),
   question: document.getElementById("question"),
   answers: document.getElementById("answers"),
   btn: document.getElementById("btn"),
+  result: document.getElementById("result"),
+  resultIcon: document.getElementById("result-icon"),
+  resultStatus: document.getElementById("result-status"),
 };
 
-//Функция рандомной пересортировки эл-ов массива;
-// arr - массив для перетасовки;
-// count - максимальное кол-во эл-ов в новом массиве;
+//Global variables
+let questionCount = 5;
+let questionIdx = 1;
+let isSelectAnswer = false;
+let rightAnswersCount = 0;
+let answersCountForTestComplete = 4;
+const newQuestionsArr = smashArr(data.questions, questionCount);
 
-const smashArray = (arr, count) => {
+//Функция рандомной пересортировки эл-ов массива;
+// randomArr - массив для перетасовки;
+// count - максимальное кол-во эл-ов в новом массиве;
+function smashArr(arr, count) {
   let randomArr = [];
 
   while (arr.length && randomArr.length < count) {
@@ -19,11 +30,9 @@ const smashArray = (arr, count) => {
     const cutQuestionArr = arr.splice(randomIdx, 1);
     randomArr.push(...cutQuestionArr);
   }
+  console.log(randomArr);
   return randomArr;
-};
-const newQuestionsArr = smashArray(data.questions, 10);
-let questionIdx = 0;
-let isSelectAnswer = false;
+}
 
 //Функция вывода ответов;
 const renderAnswers = (answers, rightAnswerNumber) => {
@@ -38,13 +47,13 @@ const renderAnswers = (answers, rightAnswerNumber) => {
       answersHtml.push(`<div class='test__answer'>${answers[i]}</div>`);
     }
   }
-  dom.answers.innerHTML = answersHtml.join(" ");
+  dom.answers.innerHTML = answersHtml.join("");
 };
 
-//Отрисовка вопроса с ответами
 const answers = newQuestionsArr[0].answers;
 const rightAnswerNumber = newQuestionsArr[0].rightAnswer;
 
+//Отрисовка вопроса с ответами
 function render(data, questionNumber) {
   const { answers, rightAnswer } = data;
   dom.question.innerHTML = data.question;
@@ -55,16 +64,22 @@ function render(data, questionNumber) {
 
 render(newQuestionsArr[0], 1);
 
+console.log(newQuestionsArr[0]);
+
 //Отслеживаем клик по кнопке перехода к следующему вопросу
 dom.btn.onclick = () => {
   const question = newQuestionsArr[questionIdx];
+  const nextQuestionIsFinish = newQuestionsArr[questionIdx + 1];
   let questionNumber = questionIdx + 1;
   if (question) {
     render(question, questionNumber);
     questionIdx++;
     isSelectAnswer = !isSelectAnswer;
+    if (!nextQuestionIsFinish) {
+      changeButton();
+    }
   } else {
-    alert("Finish");
+    renderResult();
   }
 };
 
@@ -74,14 +89,15 @@ dom.answers.onclick = (event) => {
   if (isAnswerClick && !isSelectAnswer) {
     renderAnswersStatus(event.target);
     isSelectAnswer = !isSelectAnswer;
+    blockButton(false);
   }
-  blockButton(false);
 };
 
 // Функция закраски статусов ответов
 function renderAnswersStatus(answer) {
   if (answer.dataset.valid !== undefined) {
     answer.classList.add("valid");
+    rightAnswersCount++;
   } else {
     const validAnswer = answer.parentNode.querySelector("[data-valid]");
     answer.classList.add("invalid");
@@ -96,4 +112,33 @@ function blockButton(isDisable) {
   } else {
     dom.btn.classList.remove("disable");
   }
+}
+
+// Функция смена наименования кнопки по окончании тестирования
+function changeButton() {
+  dom.btn.innerHTML = "Смотреть результат";
+  dom.btn.dataset.result = true;
+}
+
+// Функция, которая рендерит результаты тестирования
+function renderResult() {
+  dom.questionBlock.style.display = "none";
+  dom.answers.style.display = "none";
+  dom.btn.style.display = "none";
+  dom.testName.style.display = "none";
+
+  if (testCompleteIsCorrect()) {
+    dom.resultIcon.innerHTML = "🥳";
+    dom.resultStatus.innerHTML = "Тест пройден!";
+    dom.resultStatus.classList.add("valid");
+  } else {
+    dom.resultIcon.innerHTML = "🫣";
+    dom.resultStatus.innerHTML = "Тест не пройден!";
+    dom.resultStatus.classList.add("invalid");
+  }
+}
+
+//Функция проверки завершения теста
+function testCompleteIsCorrect() {
+  return answersCountForTestComplete < rightAnswerNumber;
 }
